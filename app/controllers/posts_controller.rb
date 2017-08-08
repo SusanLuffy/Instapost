01e906b85a0a51ff085c1b02c7f06ca7b6f04cea
Susan <susanfuhn@gmail.com>
2017-08-08 13:51:11 +0000

    Post Backend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,18 @@ class PostsController < ApplicationController
 def new 
    @post = Post.new 
 end
+def create
+  @post = current_user.posts.create(post_params)
+  if @post.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+before_action :authenticate_user!, only: [:new, :create]
+private
+
+def post_params
+  params.require(:post).permit(:user_id, :photo, :description)
+end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
     mount_uploader :photo, PhotoUploader
+    validates :photo, :description, :user_id, presence: true
 end
